f71e5efb37b84e51c8ab6151018dc9013c6e846e
rafapev <rafaelpereirav@outlook.com>
2022-05-02 19:56:35 -0300

    First commit Intellij

---
+++ src/br/com/dio/primeiroprograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class primeiroprograma {
+    public static void main(String[] args) {
+      gato gato = new gato();
+
+        System.out.println(gato);
+
+
+        /*int a = 5;
+        int b = 6;
+        System.out.println("Hello World " + (a+b) );*/
+    }
+}
